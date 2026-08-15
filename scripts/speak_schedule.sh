0b914277759422ce0c8ddf296f37b25e23ae2f54
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-08-15 12:20:18 +0200

    EOD

---
+++ scripts/speak_schedule.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'espeak -s 120 -p 10 "Time to open the door"' | at 13:46
+echo 'espeak -s 120 -p 10 "Time to start"' | at 14:00
+echo 'espeak -s 120 -p 10 "Time to take a break"' | at 15:00
+echo 'espeak -s 120 -p 10 "Time to go back to work"' | at 15:15
+echo 'espeak -s 120 -p 10 "Time to go home"' | at 16:00
+
+
